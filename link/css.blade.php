a6f54da5ba5d1c2dbcf3d76b0df37892b8e0b03c
AO™ <anthony@osawere.com>
2025-04-20 19:37:38 +0100

    Update css.blade.php

--- link/css.blade.php
+++ link/css.blade.php
@@ -1,10 +1,25 @@
+@use('Groy\Xeno\Data\StringX')
 @use('Groy\Xeno\Theme\AssetX')
-@props(['href', 'noscript' => false])
+@props([
+	'href',
+	'rel' => 'stylesheet',
+	'noscript' => false,
+	'preload' => false,
+	'vendor' => false
+])
 
-<link rel="preload" as="style" href="{{ AssetX::css($href) }}" onload="this.onload=null;this.rel='stylesheet'">
+@php
+	$href = $vendor ? StringX::end()->ifNot($href, '.css') : $href;
+	$finalHref = $vendor ? AssetX::vendor($href, false) : AssetX::css($href, false);
+@endphp
 
-@if($noscript)
-	<noscript>
-		<link rel="stylesheet" href="{{ AssetX::css($href) }}">
-	</noscript>
+@if($rel === 'preload' || $noscript)
+	<link rel="preload" as="style" href="{{ $finalHref }}" onload="this.onload=null;this.rel='stylesheet'">
+	@if($noscript)
+		<noscript>
+			<link rel="stylesheet" href="{{ $finalHref }}">
+		</noscript>
+	@endif
+@else
+	<link rel="{{ $rel }}" href="{{ $finalHref }}">
 @endif
