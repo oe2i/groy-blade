f932f4c335e7f7836abfa1d708beff291324e63e
AO™ <anthony@osawere.com>
2025-04-21 21:22:16 +0100

    Create link.blade.php

---
+++ chip/link.blade.php
@@ -0,0 +1,48 @@
+@use('Groy\Xeno\Http\RouteX')
+@use('Groy\Xeno\Data\StringX')
+@use('Groy\Spine\Core\VarX')
+@props([
+	'text' => null,
+	'href',
+	'param' => [],
+	'absolute' => false,
+	'class' => false,
+	'title' => true,
+	'navigate' => true,
+])
+
+@php
+	$attributes = collect();
+
+	if ($href) {
+		$href = RouteX::href($href, $param, $absolute);
+		$attributes[] = 'href="' . e($href) . '"';
+	}
+
+	if ($navigate) {
+		$attributes->push('wire:navigate');
+	}
+
+	if ($class) {
+		$attributes[] = 'class="' . e($class) . '"';
+	}
+
+	if ($slot) {
+		$content = $slot;
+	} elseif (StringX::has()->something($text)) {
+		$content = $text;
+	}
+	$content = $content ?? 'No Anchor';
+
+	if (VarX::true($title) && !VarX::html($content)) {
+		$title = $content;
+	}
+
+	if (VarX::notEmpty($title)) {
+		$attributes[] = 'title="' . e($title) . '"';
+	}
+
+	$attributes = $attributes->implode(' ');
+@endphp
+
+<a {!! $attributes !!}>{!! $content !!}</a>
