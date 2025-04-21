8dc14493941e758235b6fc07b6a4577bd529b87a
AO™ <anthony@osawere.com>
2025-04-21 21:22:21 +0100

    Update title.blade.php

--- chip/title.blade.php
+++ chip/title.blade.php
@@ -1,13 +1,11 @@
-@props([
-	'title',
-	'preload' => false
-])
+@props(['title', 'preload' => false])
 
-@if($preload)
+@if ($preload)
 	@php
-		$preload = (is_bool($preload) || empty($preload)) ? 'Loading...' : $preload;
+		$preload = is_bool($preload) || empty($preload) ? 'Loading...' : $preload;
 	@endphp
-		<title orio-title="{{ $title }}">{{ $preload }}</title>
+
+	<title orio-title="{{ $title }}">{{ $preload }}</title>
 @else
 	<title>{{ $title }}</title>
 @endif
