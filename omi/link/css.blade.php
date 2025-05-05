f7804f064f2a62e47367687516d5e6a81c642cbf
AO™ <anthony@osawere.com>
2025-05-05 13:07:11 +0100

    Update css.blade.php

--- omi/link/css.blade.php
+++ omi/link/css.blade.php
@@ -1,13 +1,24 @@
-@use ('Groy\Xeno\Skin\AssetX')
-@use ('Groy\Xeno\Data\String\EndX')
-@props(['href', 'rel' => 'stylesheet', 'noscript' => false, 'preload' => false, 'vendor' => false])
+@props(['href' => null, 'rel' => 'stylesheet', 'noscript' => false, 'preload' => false, 'vendor' => false])
 
 @php
-	$href = $vendor ? EndX::ifNot($href, '.css') : $href;
+
+	if (StrBeginX::with($href, 'orio::')) {
+		$href = 'orio/' . StrCropX::begin($href, 'orio::');
+	}
+
+	if ($href === 'orio') {
+		$href = 'orio/orio.css';
+	}
+
+	if (!$href) {
+		$href = EnvX::theme();
+	}
+
+	$href = $vendor ? StrEndX::ifNot($href, '.css') : $href;
 	$finalHref = $vendor ? AssetX::vendor($href, false) : AssetX::css($href, false);
 @endphp
 
-@if ($rel === 'preload' || $noscript)
+@if ($rel === 'preload' || $noscript || $preload)
 	<link rel="preload" as="style" href="{{ $finalHref }}" onload="this.onload=null;this.rel='stylesheet'">
 	@if ($noscript)
 		<noscript>
