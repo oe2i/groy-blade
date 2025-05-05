0eda5d201d2a2effd1f2f63aadf8264176544ae2
AO™ <anthony@osawere.com>
2025-05-05 06:12:06 +0100

    Create css.blade.php

---
+++ omi/link/css.blade.php
@@ -0,0 +1,19 @@
+@use ('Groy\Xeno\Skin\AssetX')
+@use ('Groy\Xeno\Data\String\EndX')
+@props(['href', 'rel' => 'stylesheet', 'noscript' => false, 'preload' => false, 'vendor' => false])
+
+@php
+	$href = $vendor ? EndX::ifNot($href, '.css') : $href;
+	$finalHref = $vendor ? AssetX::vendor($href, false) : AssetX::css($href, false);
+@endphp
+
+@if ($rel === 'preload' || $noscript)
+	<link rel="preload" as="style" href="{{ $finalHref }}" onload="this.onload=null;this.rel='stylesheet'">
+	@if ($noscript)
+		<noscript>
+			<link rel="stylesheet" href="{{ $finalHref }}">
+		</noscript>
+	@endif
+@else
+	<link rel="{{ $rel }}" href="{{ $finalHref }}">
+@endif
