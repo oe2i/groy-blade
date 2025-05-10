d01708050c561058b3711237898c4f141003c43d
AO™ <anthony@osawere.com>
2025-05-10 08:00:56 +0100

    Create js.blade.php

---
+++ omi/link/js.blade.php
@@ -0,0 +1,27 @@
+@props(['src' => null, 'async' => false, 'defer' => false, 'crossorigin' => null, 'type' => 'text/javascript', 'vendor' => false])
+
+@php
+	$attributes = OrioX::initAttr();
+	OrioX::setAttr(compact('type'), $attributes);
+	OrioX::setAttr(compact('async'), $attributes);
+	OrioX::setAttr(compact('defer'), $attributes);
+	if ($crossorigin === true || $crossorigin === 'true') {
+		$crossorigin = 'anonymous';
+	}
+	OrioX::setAttr(compact('crossorigin'), $attributes);
+	OrioX::toAttr($attributes);
+
+	if (!$src) {
+		$src = EnvX::theme();
+	}
+	$orioSrc = OrioX::asset($src);
+	if ($orioSrc) {
+		$finalSrc = $orioSrc;
+	} else {
+		$finalSrc = $vendor ? AssetX::vendor($src, false) : AssetX::js($src, false);
+	}
+
+	$finalSrc = StrEndX::ifNot($finalSrc, '.js');
+@endphp
+
+<script {!! $attributes !!} src="{{ $finalSrc }}"></script>
