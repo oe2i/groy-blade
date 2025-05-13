0b25a5558ae5c4a9547ab1fc193d18354b6df8ff
AO™ <anthony@osawere.com>
2025-05-13 01:09:46 +0100

    Update site.blade.php

--- allo/layout/site.blade.php
+++ allo/layout/site.blade.php
@@ -1,11 +1,19 @@
-@props(['title', 'page' => null, 'heading' => null])
+@props(['title' => null, 'page' => null, 'heading' => null])
 
 <!DOCTYPE html>
 <html lang="en" o-theme="dark">
 
 	<head>
 
-		<x-orio.orio.meta.chars />
+		<x-groy.orio.meta.chars />
+		<x-groy.orio.meta.viewport />
+		<x-groy.orio.meta.compatible />
+		<x-groy.orio.meta.csrf />
+		<x-groy.orio.elem.title :title=$title />
+		<x-groy.orio.chunk.favicon type='png' mask="#222288" microsoft="#AAAAFF" />
+		<x-groy.orio.link.css href="groy::rebase" />
+		<x-groy.orio.link.css href="groy::theme" />
+		<x-groy.orio.link.css href="orio" />
 
 	</head>
 
@@ -14,7 +22,7 @@
 		<header>
 			<div class="container">
 				<div id="logo">
-					Logo!
+					{{ ProjectX::alias() }}
 				</div>
 
 				<nav>
@@ -22,7 +30,7 @@
 						<li><a href="{{ RouteX::href('home') }}">home</a></li>
 						<li><a href="{{ RouteX::href('about') }}">about</a></li>
 						<li><a href="{{ RouteX::href('contact') }}">contact</a></li>
-						<li><a href="{{ RouteX::href('html') }}">html</a></li>
+						<li><a href="{{ RouteX::href('html') }}">HTML</a></li>
 
 						@auth
 							@if (Route::has('dashboard'))
