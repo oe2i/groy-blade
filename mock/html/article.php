f0af66069ef2a4c9f3991eeef2ff9b5f4669cc16
AO™ <anthony@osawere.com>
2025-05-13 21:41:24 +0100

    Create article.php

---
+++ mock/html/article.php
@@ -0,0 +1,37 @@
+<section>
+	<h2>Semantic & Structural</h2>
+	<article>
+		<header>
+			<h3>Article Title</h3>
+		</header>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id auctor risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue diam, congue at fermentum eu, fermentum sed quam. Curabitur a metus ante. Suspendisse potenti. Curabitur lacinia nulla mollis ipsum hendrerit mattis. Donec eget quam sit amet mi tempus fringilla in eget nunc. Morbi commodo volutpat ullamcorper.</p>
+		<p>Donec iaculis dui metus, eget condimentum est lobortis sit amet. Donec fermentum, lacus ut aliquet sodales, urna risus sagittis purus, non mollis purus massa eu odio. Suspendisse gravida nec nisi et lacinia. Aliquam pellentesque sapien vitae sapien egestas, mollis tristique augue mattis. Proin ac quam nec turpis accumsan interdum. Curabitur vestibulum nisl in congue pretium. Pellentesque ut dictum dolor. Mauris vitae arcu aliquam, fermentum justo eu, gravida urna. Nam vitae ante ipsum. Ut condimentum felis ut eleifend facilisis. Nunc tempus aliquam dictum. Aliquam egestas eros nibh, at fringilla sem rhoncus sed. Nulla facilisi. Phasellus ac accumsan nisl. Proin ultricies dapibus urna in elementum. Interdum et malesuada fames ac ante ipsum primis in faucibus. </p>
+		<footer>Lorem Ipsum Footer - <a href="#headings">Go to Headings</a></footer>
+	</article>
+
+	<aside>
+		<h3>The Aside!</h3>
+		<p>Nam efficitur sem nec ullamcorper mattis. Donec ut mi diam. Phasellus efficitur in arcu quis pulvinar. Suspendisse elementum dui nec luctus molestie. Nam felis eros, mollis vitae eleifend in, vehicula quis mauris. Quisque nulla eros, dignissim in lorem eget, mollis ultrices urna. Fusce venenatis elit ac odio condimentum, quis elementum sapien accumsan. </p>
+	</aside>
+
+	<h2>Cities</h2>
+	<details name="cities">
+		<summary>List of Cities</summary>
+		<ul>
+			<li>Benin</li>
+			<li>Lagos</li>
+			<li>Abuja</li>
+			<li>FCT</li>
+
+			<li>Warri</li>
+			<li>Kaduna</li>
+		</ul>
+		<p><em>Everything here is hidden until toggled</em></p>
+	</details>
+
+	<dialog open>This is a dialog.</dialog>
+	<blockquote cite="https://example.com">
+		“This is a blockquote from a source.”
+	</blockquote>
+	<address>123 Web Ave, HTML City</address>
+</section>
