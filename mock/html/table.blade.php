9ed5f6be474e6c699f89053a9c41281b2c125975
AO™ <anthony@osawere.com>
2025-05-13 21:47:15 +0100

    Create table.blade.php

---
+++ mock/html/table.blade.php
@@ -0,0 +1,27 @@
+	<section>
+		<h2>Table</h2>
+		<table>
+			<caption>Sample Table</caption>
+			<thead>
+				<tr>
+					<th>Header 1</th>
+					<th>Header 2</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Cell 1</td>
+					<td>Cell 2</td>
+				</tr>
+				<tr>
+					<td>Cell 3</td>
+					<td>Cell 4</td>
+				</tr>
+			</tbody>
+			<tfoot>
+				<tr>
+					<td colspan="2">Footer</td>
+				</tr>
+			</tfoot>
+		</table>
+	</section>
