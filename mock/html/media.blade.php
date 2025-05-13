1a12b3676533c2ca06268d6460c0ab37a5675223
AO™ <anthony@osawere.com>
2025-05-13 21:47:18 +0100

    Create media.blade.php

---
+++ mock/html/media.blade.php
@@ -0,0 +1,16 @@
+<section>
+	<h2>Media</h2>
+	<img src="//picsum.photos/200" alt="Placeholder image">
+	<figure>
+		<img src="//picsum.photos/200?random=2" alt="Figure Image">
+		<figcaption>Figure caption</figcaption>
+	</figure>
+	<audio controls>
+		<source src="//samplelib.com/lib/preview/mp3/sample-15s.mp3" type="audio/mpeg">
+		Your browser does not support the audio element.
+	</audio>
+	<video width="320" height="240" controls>
+		<source src="//commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4">
+		Your browser does not support the video tag.
+	</video>
+</section>
