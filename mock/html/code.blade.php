399026bac601a9917b85a9272626fe48e729c0e5
AO™ <anthony@osawere.com>
2025-05-13 21:41:26 +0100

    Create code.blade.php

---
+++ mock/html/code.blade.php
@@ -0,0 +1,10 @@
+<section>
+	<h2>Code & Pre</h2>
+	<code>Inline code</code>
+	<pre>
+		function test() {
+			console.log('Hello World');
+		}
+		</pre>
+	<kbd>Ctrl + S</kbd>, <samp>Sample Output</samp>, <var>x</var>
+</section>
