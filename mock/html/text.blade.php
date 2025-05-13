9f096905a36c94d717f9522d7671445e022ab180
AO™ <anthony@osawere.com>
2025-05-13 21:47:21 +0100

    Create text.blade.php

---
+++ mock/html/text.blade.php
@@ -0,0 +1,23 @@
+<section>
+	<h2>Text Content</h2>
+	<p>
+		This is a paragraph with
+		<strong>strong</strong>,
+		<em>em</em>,
+		<b>bold</b>,
+		<i>italic</i>,
+		<u>underline</u>,
+		<small>small</small>, and
+		<mark>mark</mark> text.
+	</p>
+	<p>
+		Subscript: H<sub>2</sub>O, <br>
+		Superscript: E=mc<sup>2</sup>
+	</p>
+	<abbr title="Hypertext Markup Language">HTML</abbr>,
+	<cite>Citation</cite>,
+	<dfn>Definition</dfn>
+	<time datetime="2025-01-01">January 1, 2025</time>
+	<br>
+	<hr>
+</section>
