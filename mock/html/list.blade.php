c2abe159d6c8dc32339c00d3b3a7df0d871ee237
AO™ <anthony@osawere.com>
2025-05-13 21:47:12 +0100

    Create list.blade.php

---
+++ mock/html/list.blade.php
@@ -0,0 +1,17 @@
+<section>
+	<h2>Lists</h2>
+	<ul>
+		<li>Unordered Item</li>
+		<li>Another Unordered Item</li>
+	</ul>
+	<ol>
+		<li>Ordered Item</li>
+		<li>Another Ordered Item</li>
+	</ol>
+
+	<h3>Definition List</h3>
+	<dl>
+		<dt>Term</dt>
+		<dd>Description</dd>
+	</dl>
+</section>
