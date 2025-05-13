43b348277dc76bbb04a7653067fd549cb25cfab8
AO™ <anthony@osawere.com>
2025-05-13 21:46:44 +0100

    Create header.blade.php

---
+++ mock/html/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+	<h1>HTML Elements</h1>
+	<em>A simple tagline with anchors to  <a href="/">home</a> & <a href="/about">about</a></em>
+	<nav>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/contact">Contact</a></li>
+			<li><a href="#headings">Heading</a></li>
+			<li><a href="#footer">Bottom</a></li>
+			<li><a href="javascript:void(0)" onclick="theme.toggle()">Toggle</a></li>
+		</ul>
+	</nav>
+</header>
